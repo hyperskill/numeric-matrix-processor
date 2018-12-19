50f2e3beccdf4d95ef3d8bab3c94e0000b4d3950
aleks <alekseyvy@gmail.com>
2018-12-19 15:02:50 +0300

    Stage 6 Completed.

--- src/matrix/Matrix.java
+++ src/matrix/Matrix.java
@@ -16,6 +16,7 @@ class Matrix {
         System.out.println("3. Multiply matrices");
         System.out.println("4. Transpose matrix");
         System.out.println("5. Calculate a determinant");
+        System.out.println("6. Inverse matrix");
         System.out.println("0. Exit");
         System.out.print("Your choice: ");
         int choice = scan.nextInt();
@@ -34,6 +35,9 @@ class Matrix {
                 break;
             case 5:
                 determinantInputMatrix();
+                break;
+            case 6:
+                InverseMatrix();
                 break;
             case 0:
                 System.exit(0);
@@ -205,15 +209,15 @@ class Matrix {
         }
         int[][] transposedMatrix = new int[col][row];
         //loop for transposing Matrix by Main diagonal
-        for(int i =0; i < matrix.length; i++){
+        for (int i = 0; i < matrix.length; i++) {
             System.out.println();
-            for(int j = 0; j < matrix[i].length; j++){
+            for (int j = 0; j < matrix[i].length; j++) {
                 transposedMatrix[j][i] = matrix[i][j];
             }
         }
-        for(int i =0; i < matrix.length; i++){
+        for (int i = 0; i < matrix.length; i++) {
             System.out.println();
-            for(int j = 0; j < matrix[i].length; j++){
+            for (int j = 0; j < matrix[i].length; j++) {
                 System.out.print(transposedMatrix[i][j] + " ");
             }
         }
@@ -236,8 +240,8 @@ class Matrix {
         }
         int[][] transposeSide = new int[row][col];
         //loop for transposing Matrix by Side diagonal
-        for (int i = 0; i < matrix.length; i++){
-            for (int j = 0; j < matrix.length; j++){
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
             }
         }
     }
@@ -257,7 +261,7 @@ class Matrix {
             }
         }
         //loop for transposing Matrix by  Vertical
-        for(int i = 0; i < matrix.length; i++){
+        for (int i = 0; i < matrix.length; i++) {
             Arrays.sort(matrix[i], Collections.reverseOrder());
         }
         for (int i = 0; i < matrix.length; i++) {
@@ -286,7 +290,7 @@ class Matrix {
         int[][] transposeHorisontal = new int[row][];
         //loop for transposing Matrix by  Horizontal
         int counter = 1;
-        for(int i = 0; i < matrix.length; i++){
+        for (int i = 0; i < matrix.length; i++) {
             transposeHorisontal[matrix.length - counter] = matrix[i];
             if (counter < matrix.length) {
                 counter++;
@@ -299,13 +303,14 @@ class Matrix {
             }
         }
     }
+
     //Method for calculation of determinant of Matrix.
     public static BigInteger determinant(final int[][] matr) {
 
         int n = matr.length;
         BigInteger[][] a = new BigInteger[n][n];
         for (int i = 0; i < n; ++i) {
-            for (int j = 0;j  < n; ++j) {
+            for (int j = 0; j < n; ++j) {
                 a[i][j] = BigInteger.valueOf(matr[i][j]);
             }
         }
@@ -355,7 +360,7 @@ class Matrix {
         return det;
     }
 
-    public static void determinantInputMatrix(){
+    public static void determinantInputMatrix() {
         System.out.print("Enter size of matrix: ");
         //rows and cols of matrix
         int row = scan.nextInt();
@@ -370,5 +375,112 @@ class Matrix {
             }
         }
         System.out.println(determinant(matrix));
+    }
+
+    public static void InverseMatrix() {
+        {
+            Scanner input = new Scanner(System.in);
+            System.out.println("Enter the dimension of square matrix: ");
+            int n = input.nextInt();
+            double a[][] = new double[n][n];
+            System.out.println("Enter the elements of matrix: ");
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                    a[i][j] = input.nextDouble();
+
+            double d[][] = invert(a);
+
+            System.out.println("The inverse is: ");
+            for (int i = 0; i < n; ++i) {
+                for (int j = 0; j < n; ++j) {
+                    System.out.print(d[i][j] + "  ");
+                }
+                System.out.println();
+            }
+            input.close();
+        }
+    }
+
+    public static double[][] invert(double a[][]) {
+        int n = a.length;
+        double x[][] = new double[n][n];
+        double b[][] = new double[n][n];
+        int index[] = new int[n];
+        for (int i = 0; i < n; ++i)
+            b[i][i] = 1;
+
+        // Transform the matrix into an upper triangle
+        gaussian(a, index);
+
+        // Update the matrix b[i][j] with the ratios stored
+        for (int i = 0; i < n - 1; ++i)
+            for (int j = i + 1; j < n; ++j)
+                for (int k = 0; k < n; ++k)
+                    b[index[j]][k]
+                            -= a[index[j]][i] * b[index[i]][k];
+
+        // Perform backward substitutions
+        for (int i = 0; i < n; ++i) {
+            x[n - 1][i] = b[index[n - 1]][i] / a[index[n - 1]][n - 1];
+            for (int j = n - 2; j >= 0; --j) {
+                x[j][i] = b[index[j]][i];
+                for (int k = j + 1; k < n; ++k) {
+                    x[j][i] -= a[index[j]][k] * x[k][i];
+                }
+                x[j][i] /= a[index[j]][j];
+            }
+        }
+        return x;
+    }
+
+// Method to carry out the partial-pivoting Gaussian
+// elimination.  Here index[] stores pivoting order.
+
+    public static void gaussian(double a[][], int index[]) {
+        int n = index.length;
+        double c[] = new double[n];
+
+        // Initialize the index
+        for (int i = 0; i < n; ++i)
+            index[i] = i;
+
+        // Find the rescaling factors, one from each row
+        for (int i = 0; i < n; ++i) {
+            double c1 = 0;
+            for (int j = 0; j < n; ++j) {
+                double c0 = Math.abs(a[i][j]);
+                if (c0 > c1) c1 = c0;
+            }
+            c[i] = c1;
+        }
+
+        // Search the pivoting element from each column
+        int k = 0;
+        for (int j = 0; j < n - 1; ++j) {
+            double pi1 = 0;
+            for (int i = j; i < n; ++i) {
+                double pi0 = Math.abs(a[index[i]][j]);
+                pi0 /= c[index[i]];
+                if (pi0 > pi1) {
+                    pi1 = pi0;
+                    k = i;
+                }
+            }
+
+            // Interchange rows according to the pivoting order
+            int itmp = index[j];
+            index[j] = index[k];
+            index[k] = itmp;
+            for (int i = j + 1; i < n; ++i) {
+                double pj = a[index[i]][j] / a[index[j]][j];
+
+                // Record pivoting ratios below the diagonal
+                a[index[i]][j] = pj;
+
+                // Modify other elements accordingly
+                for (int l = j + 1; l < n; ++l)
+                    a[index[i]][l] -= pj * a[index[j]][l];
+            }
+        }
     }
 }
